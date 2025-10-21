cd559e32ecb5d40b26dfb72a7a08b535e5764e57
hmweb77 <oussamahamdani1@gmail.com>
2025-10-21 14:44:17 +0100

    adding analyrics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
+
+import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,6 +22,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        {/* Google Analytics Script */}
+
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-86KL8KF298"
+        ></Script>
+        <Script>
+          {` window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-86KL8KF298');   `}
+        </Script>
+      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
